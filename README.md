f3bf017c130c143d8d686dafe11e784a0b6ed439
isloth <insensak.007@gmail.com>
2018-08-18 14:20:35 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Install Server Minecraft PE 1.1.5 on Linux
+
+##By [iBoy Sloth-](https://youtube.com/iboysloth)
+
+# iCraft-MCPE
---
+++ start.py
@@ -0,0 +1,19 @@
+import os, sys
+
+os.system("apt install figlet -y")
+os.system("cd")
+os.system("clear")
+os.system("figlet Start Install")
+print "========================="
+print "Minecraft PE 1.1.5 Server"
+print "By : iBoY Sloth-"
+print "========================="
+os.system("wget get.pmmp.io")
+os.system("bash index.html -r")
+os.system("rm PocketMine-MP.phar")
+os.system("wget https://genisyspro.github.io/GenisysPro/GenisysPro.phar")
+os.system("mv GenisysPro.phar PocketMine-MP.phar")
+os.system("clear")
+os.system("figlet Done! Install")
+print "Run your server use ./start.sh"
+os.system("rm start.py")
